4fa8a54880d490fd865fdfbd69540f3ff9ee5652
VenusBridget <venusbri27@gmail.com>
2024-04-14 12:34:50 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Venus is the champion";
+document.body.append (newHeader)
+
+
